8a779fb810fee5cd74004f9dea43aba80a23b36d
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2018-01-20 22:47:16 +0000

    Removed app stravaAuth

--- stravaAuth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StravaauthConfig(AppConfig):
-    name = 'stravaAuth'
--- stravaAuth/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Key(models.Model):
-    email = models.TextField(default='')
-    text = models.TextField(default='')
-
