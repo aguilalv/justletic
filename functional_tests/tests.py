6ac335e6cc0369663cb4eafba2bd1d98dc51e0fa
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2018-01-21 13:08:36 +0000

    Added mew functional test to support several users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,7 +27,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_authorise_a_strava_accoun(self):
+    def test_can_authorise_a_strava_account(self):
         # Edith has heard about a cool new online training app. She goes
         # to check out its homepge
         self.browser.get(self.live_server_url)
@@ -47,13 +47,12 @@ class NewVisitorTest(LiveServerTestCase):
         # She types "edith@mailinator.com"" into a text box
         inputbox.send_keys('edith@mailinator.com')
 
-        # When she hits enter, she sees her email and strava key
+        # When she hits enter, she sees her email and Strava key 
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_keys_table('edith@mailinator.com')
-        self.wait_for_row_in_keys_table('123456')
-        self.fail('Finish the test!')
-
-
+        self.wait_for_row_in_keys_table('e1234')
+        
+        
         # When she hits enter, she is redirected to a Strava page to authorise
         # accessing some of her data
 
@@ -63,3 +62,47 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied she goes to sleep
 
+    def test_multiple_users_show_different_key_lists_at_different_urls(self):
+        # Edith authenticates Justletic to access her Strava data
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_email_in')
+        inputbox.send_keys('edith@mailinator.com')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_keys_table('edith@mailinator.com')
+        self.wait_for_row_in_keys_table('e1234')
+
+        # She notices that her summary page has a unique URL
+        edith_summary_url = self.browser.current_url
+        self.assertRegex(edith_summary_url, '/users/.+')
+
+        # Now a new user, Francis, comes along to the site
+
+        ## Use new browser session to make sure no information of previous
+        ## user coming through cookies, etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith´s keys
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.asserNotIn('edith@mailinator.com', page_text)
+        self.asserNotIn('e1234', page_text)
+
+        # Francis authenticates his own Strava account
+        inputbox = self.browser.find_element_by_id('id_email_in')
+        inputbox.send_keys('francis@mailinator.com')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_keys_table('francis@mailinator.com')
+        self.wait_for_row_in_keys_table('f1234')
+
+        # Francis gets his own unique URL
+        francis_summary_url = self.browser.current_url
+        self.assertRegex(francis_summary_url, '/users/.+')
+        self.assertNotEqual(francis_summary_url, edith_summary_url)
+
+        # Again, there is no trace of Edith´s keys
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.asserNotIn('edith@mailinator.com', page_text)
+        self.asserNotIn('e1234', page_text)
+
+        # Satisfied, they both go back to sleep
