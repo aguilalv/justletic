1e8e4a665a4a8e3faea35324a9882774377d01cb
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2018-02-24 20:46:33 +0000

    Refactored Unit Tests into separate files for views and models under tests folder

--- keys/tests/test_views.py
+++ keys/tests/test_views.py
@@ -1,8 +1,6 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
-from .models import Key,User
+from ..models import Key,User
 
 class HomePageTest(TestCase):
 
@@ -89,33 +87,3 @@ class NewServiceTest(TestCase):
         )
 
         self.assertRedirects(response,f'/users/{correct_user.id}/')
-
-class UserAndKeyModelTest(TestCase):
-
-    def test_saving_and_retrieving_keys(self):
-        user = User()
-        user.save()
-
-        first_key = Key()
-        first_key.value = 'First'
-        first_key.user = user
-        first_key.save()
-
-        second_key = Key()
-        second_key.value = 'Second'
-        second_key.user = user
-        second_key.save()
-
-        saved_user = User.objects.all()[0]
-        self.assertEqual(saved_user, user)
-
-        saved_keys = Key.objects.all()
-        self.assertEqual(saved_keys.count(), 2)
-
-        first_saved_key = saved_keys[0]
-        second_saved_key = saved_keys[1]
-        self.assertEqual(first_saved_key.value, first_key.value)
-        self.assertEqual(first_saved_key.user, first_key.user)
-        self.assertEqual(second_saved_key.value, second_key.value)
-        self.assertEqual(second_saved_key.user, second_key.user)
-
