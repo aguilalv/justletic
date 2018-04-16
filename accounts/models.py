0d45dcd95aeebbf957f60bce67136aaa441c14b5
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2018-04-16 22:42:46 +0100

    Refactored account models to comply with pylint guidelines

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,14 @@
+""" Models to store Justletic user accounts """
+
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, PermissionsMixin
 )
 
 class User(AbstractBaseUser, PermissionsMixin):
+
+    """Tests for accounts models"""
+
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
