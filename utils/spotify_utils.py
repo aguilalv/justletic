45ae4609cd733db4861ffbf9592662d59fe918dd
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2018-08-27 23:10:21 +0100

    Added helper function to return url to request spotify code to Spotify utils

--- utils/spotify_utils.py
+++ utils/spotify_utils.py
@@ -9,6 +9,16 @@ SPOTIFY_CLIENT_ID = '1aaa5ce0611f42cea3b4eeff885b807d'
 SPOTIFY_CODE_EXCHANGE_URL = 'https://accounts.spotify.com/api/token' 
 SPOTIFY_AUTHORIZE_URL = 'https://accounts.spotify.com/authorize'
 
+def spotify_oauth_code_request_url():
+    parameters_dict = {
+        'client_id': SPOTIFY_CLIENT_ID,
+        'redirect_uri': os.environ['SPOTIFY_REDIRECT_URI'], 
+        'response_type': 'code',
+        'scope': 'user-read-recently-played user-top-read'
+    }
+    parameters = urlencode(parameters_dict)
+    return f'{SPOTIFY_AUTHORIZE_URL}?{parameters}'
+
 def request_spotify_oauth_code():
     parameters = {
         'client_id': SPOTIFY_CLIENT_ID,
--- utils/tests/test_spotify_utils.py
+++ utils/tests/test_spotify_utils.py
@@ -7,6 +7,7 @@ from urllib.parse import parse_qsl, urlparse, parse_qs
 from django.test import TestCase
     
 from utils.spotify_utils import (
+    spotify_oauth_code_request_url,
     request_spotify_oauth_code,
     exchange_spotify_code,
 )
@@ -142,3 +143,49 @@ class ExchangeSpotifyCode(TestCase):
         token_received,refresh_token_received = exchange_spotify_code(code='abc123')
         self.assertIsNone(token_received)
         self.assertIsNone(refresh_token_received)
+
+class SpotifyOAuthCodeRequestUrl(TestCase):
+    
+    """Unit Tests for helper function that returns oAuth Code
+    request URL for Spotify"""
+
+    def test_returns_spotify_authorization_url(self):
+        """ Test that StravaOAuthCodeRequest helper function 
+        returns the url to request a code to Spotify"""
+        expected_parameters = {
+            'client_id': SPOTIFY_CLIENT_ID,
+            'redirect_uri': os.environ['SPOTIFY_REDIRECT_URI'], 
+            'response_type': 'code',
+            'scope': 'user-read-recently-played user-top-read'
+        }
+        url = urlparse(
+            spotify_oauth_code_request_url()
+        )
+        parameters = parse_qs(url.query)
+        
+        self.assertEqual(
+            SPOTIFY_AUTHORIZE_URL,
+            f'{url.scheme}://{url.netloc}{url.path}'
+        )
+        self.assertEqual(
+            len(expected_parameters.keys()),    
+            len(parameters.keys())
+        )
+        self.assertEqual(
+            expected_parameters['client_id'],
+            parameters['client_id'][0]
+        )
+        self.assertEqual(
+            expected_parameters['redirect_uri'],
+            parameters['redirect_uri'][0]
+        )
+        self.assertEqual(
+            expected_parameters['response_type'],
+            parameters['response_type'][0]
+        )
+        self.assertEqual(
+            expected_parameters['scope'],
+            parameters['scope'][0]
+        )
+
+
