064d6d584fd818c1e22c2ca2fb00cac551c31251
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2018-08-27 20:02:43 +0100

    Added helper function to exchange code for token and tests

--- utils/spotify_utils.py
+++ utils/spotify_utils.py
@@ -6,7 +6,7 @@ from urllib.parse import urlencode
 
 SPOTIFY_AUTH_ERROR = "Oops, something went wrong asking Spotify about you ..."
 SPOTIFY_CLIENT_ID = '1aaa5ce0611f42cea3b4eeff885b807d'
-SPOTIFY_CODE_EXCHANGE_URL = 'https://www.strava.com/oauth/token' 
+SPOTIFY_CODE_EXCHANGE_URL = 'https://accounts.spotify.com/api/token' 
 SPOTIFY_AUTHORIZE_URL = 'https://accounts.spotify.com/authorize'
 
 def request_spotify_oauth_code():
@@ -17,3 +17,22 @@ def request_spotify_oauth_code():
         'scope': 'user-read-recently-played user-top-read'
     }
     response = requests.post(SPOTIFY_AUTHORIZE_URL, parameters)
+
+def exchange_spotify_code(code):
+    data = {
+        'client_id': SPOTIFY_CLIENT_ID,
+        'client_secret': os.environ['SPOTIFY_CLIENT_SECRET'], 
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': os.environ['SPOTIFY_REDIRECT_URI'], 
+    }
+    
+    response = requests.post(SPOTIFY_CODE_EXCHANGE_URL, data = data) 
+   
+    if response.status_code != 200:
+        return (None, None)
+    else:
+        data_received = response.json()
+        token_received = data_received.get('access_token')
+        refresh_token_received = data_received.get('refresh_token')
+        return (token_received, refresh_token_received)
--- utils/tests/test_spotify_utils.py
+++ utils/tests/test_spotify_utils.py
@@ -8,7 +8,7 @@ from django.test import TestCase
     
 from utils.spotify_utils import (
     request_spotify_oauth_code,
-#    exchange_spotify_code,
+    exchange_spotify_code,
 )
 from utils.spotify_utils import (
     SPOTIFY_AUTHORIZE_URL, 
@@ -55,3 +55,90 @@ class RequestSpotifyOAuthCodeTest(TestCase):
             sent_parameters,
             expected_parameters
         )
+
+class ExchangeSpotifyCode(TestCase):
+
+    """Unit tests for helper function that exchanges Spotify code for Token"""
+
+    def register_token_exchange_url_in_httpretty(self):
+        mock_body = (
+            '{'
+                '"access_token": "NgCXRKMxYjw",'
+                '"token_type": "Bearer",'
+                '"scope": "user-read-recently-played user-top-read",'
+                '"expires_in": 3600,'
+                '"refresh_token": "NgAagAUm_SHo"'
+            '}'
+        )
+        httpretty.register_uri(
+            httpretty.POST,
+            SPOTIFY_CODE_EXCHANGE_URL,
+            body = mock_body
+        )
+
+    def register_token_exchange_url_in_httpretty_return_error(self):
+        httpretty.register_uri(
+            httpretty.POST,
+            SPOTIFY_CODE_EXCHANGE_URL,
+            body = {},
+            status = 400
+        )
+    
+    @httpretty.activate
+    def test_sends_get_request_with_code_received(self):
+        """Test that exchange spotify token helper function sends get
+        request to exchange code for token"""
+        self.register_token_exchange_url_in_httpretty()
+        
+        expected_body_parameters = {
+            'client_id': SPOTIFY_CLIENT_ID,
+            'client_secret': os.environ['SPOTIFY_CLIENT_SECRET'], 
+            'grant_type': 'authorization_code',
+            'code': 'abc123',
+            'redirect_uri': os.environ['SPOTIFY_REDIRECT_URI'], 
+        }
+       
+        exchange_spotify_code(code='abc123')
+        
+        self.assertNotIsInstance(
+            httpretty.last_request(),
+            httpretty.HTTPrettyRequestEmpty
+        )
+
+        requested_url = 'https://'+\
+            httpretty.last_request().headers.get('Host') +\
+            httpretty.last_request().path
+        sent_parameters = dict(
+            parse_qsl(httpretty.last_request().body.decode())
+        )
+        self.assertEqual(SPOTIFY_CODE_EXCHANGE_URL,requested_url) 
+
+        request_body = httpretty.last_request().parse_request_body(httpretty.last_request().body)
+        
+        for key in expected_body_parameters:
+            self.assertEqual(
+                expected_body_parameters[key],
+                request_body[key][0]
+            )
+    
+    @httpretty.activate
+    def test_returns_token_and_refresh_token(self):
+        self.register_token_exchange_url_in_httpretty()
+        
+        token_received,refresh_token_received = exchange_spotify_code(code='abc123')
+
+        self.assertEqual(
+            token_received,
+            "NgCXRKMxYjw"
+        )
+        self.assertEqual(
+            refresh_token_received,
+            "NgAagAUm_SHo"
+        )
+    
+    @httpretty.activate
+    def test_returns_none_and_none_when_error(self):
+        self.register_token_exchange_url_in_httpretty_return_error()
+        token_received,refresh_token_received = exchange_spotify_code(code='abc123')
+        self.assertIsNone(token_received)
+        self.assertIsNone(refresh_token_received)
